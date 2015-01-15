1188576f607f8b8e5573c2c782978d532530e820
Matti Schneider <matti.schneider@data.gouv.fr>
2015-01-15 10:57:40 +0100

    Allow updates to the status

--- client/views/fiches/item.js
+++ client/views/fiches/item.js
@@ -6,3 +6,15 @@ Template.fichesItem.helpers({
 		}[status];
 	}
 });
+
+Template.fichesItem.events({
+	'click .status': function(event, template) {
+		var statuses = Fiches.simpleSchema()._schema.status.allowedValues,
+			currentStatus = template.data.status;
+			currentStatusIndex = statuses.indexOf(currentStatus);
+
+		var newStatus = statuses[(currentStatusIndex + 1) % statuses.length];
+
+		Fiches.update(template.data._id, { $set: { status: newStatus } });
+	}
+});
