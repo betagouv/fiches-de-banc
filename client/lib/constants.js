7cff5e181daad91fb251e9a6c7c6b0e0ad7b49f5
Matti Schneider <matti.schneider@data.gouv.fr>
2015-01-15 19:13:19 +0100

    Correct app title

--- client/lib/constants.js
+++ client/lib/constants.js
@@ -3,8 +3,3 @@
 if (Meteor.App) {
   throw new Meteor.Error('Meteor.App already defined? see client/lib/constants.js');
 }
-
-Meteor.App = {
-  NAME: 'Meteor Boilerplate',
-  DESCRIPTION: 'A boilerplate for meteorjs projects http://matteodem.github.io/meteor-boilerplate/'
-};
