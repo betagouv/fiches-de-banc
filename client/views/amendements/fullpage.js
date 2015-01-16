cad907f407681fc048229c1cdc1fe46f1ae31769
Matti Schneider <matti.schneider@data.gouv.fr>
2015-01-16 14:34:28 +0100

    Add next/previous Amendement buttons in full page

--- client/views/amendements/fullpage.js
+++ client/views/amendements/fullpage.js
@@ -2,6 +2,16 @@ Template.amendementsFullpage.helpers({
 	article: function() {
 		return Template.instance().data.content.place.replace(/ ?Article ?/i, '')
 	},
+	next: function() {
+		return Amendements.findOne({
+			'content.position': Template.instance().data.content.position + 1
+		});
+	},
+	previous: function() {
+		return Amendements.findOne({
+			'content.position': Template.instance().data.content.position - 1
+		});
+	},
 	_: function(position) {
 		return {
 			'positive': 'Favorable',
