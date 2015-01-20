7ab95314d76959b175d7dc05ff79e03838a8b582
Matti Schneider <matti.schneider@data.gouv.fr>
2015-01-20 16:23:52 +0100

    Display only user’s Amendements on login

--- client/views/meta/login.js
+++ client/views/meta/login.js
@@ -3,7 +3,15 @@ Template.login.created = Meteor.logout;
 
 Template.login.events({
 	'submit': function(event, template) {
-		Meteor.loginWithPassword(template.find('[name="email"]').value, template.find('[name="password"]').value, Router.go.bind(Router, template.find('form').action));
+		Meteor.loginWithPassword(
+			template.find('[name="email"]').value,
+			template.find('[name="password"]').value,
+			function() {
+				Router.go('amendementsForUser', {
+					_id: Meteor.userId()
+				});
+			}
+		);
 		event.preventDefault();
 	}
 });
