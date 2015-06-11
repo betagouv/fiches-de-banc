7babe99a09199bce1f020a2e49e5014f8af51aac
Matti Schneider <hi@mattischneider.fr>
2015-06-11 22:12:20 +0200

    Update Meteor to v1.1.0

--- client/views/components/lastUpdate.js
+++ client/views/components/lastUpdate.js
@@ -1,8 +1,8 @@
-Template.lastUpdate.created = function() {
-	this.timer = new Deps.Dependency();
+Template.lastUpdate.onCreated(function() {
+	this.timer = new Tracker.Dependency();
 
 	window.setInterval(this.timer.changed.bind(this.timer), 5000);	// ensure formattedDate helper reruns
-}
+});
 
 Template.lastUpdate.helpers({
 	format: function(date) {
