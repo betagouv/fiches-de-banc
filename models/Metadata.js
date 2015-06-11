acb10f10ee1d4817411937eb850edf3c7e5f9c09
Matti Schneider <hi@mattischneider.fr>
2015-06-11 22:13:12 +0200

    Minor readability improvement

--- models/Metadata.js
+++ models/Metadata.js
@@ -1,14 +1,13 @@
 Metadata = new Mongo.Collection('Metadata');
 
-
-function yes() {
-	return true;
-}
-
 if (Meteor.isServer) {
 	Metadata.deny({
 		insert: yes,
 		update: yes,
 		remove: yes
 	});
+}
+
+function yes() {
+	return true;
 }
