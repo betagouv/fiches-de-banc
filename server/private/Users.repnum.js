2dbf4a66945eff77ca744e47105f50973e3a88c4
Matti Schneider <matti.schneider@data.gouv.fr>
2016-03-24 12:03:53 +0100

    Add real data

---
+++ server/private/Users.repnum.js
@@ -0,0 +1,43 @@
+USERS = typeof USERS !== 'undefined' ? USERS : [];
+
+USERS = USERS.concat([
+	{
+		"username": "Laure Lucchesi",
+		"email": "laure.lucchesi@modernisation.gouv.fr",
+		"password": "repnum2016"
+	}, {
+		"username": "Perica Sucevic",
+		"email": "perica.sucevic@modernisation.gouv.fr",
+		"password": "repnum2016"
+	}, {
+		"username": "Alexandre Tisserant",
+		"email": "alexandre.tisserant@cabinets.finances.gouv.fr",
+		"password": "repnum2016"
+	}, {
+		"username": "Bertrand Pailhes",
+		"email": "bertrand.pailhes@cabinets.finances.gouv.fr",
+		"password": "repnum2016"
+	}, {
+		"username": "Claire Ponty",
+		"email": "claire.ponty@cabinets.finances.gouv.fr",
+		"password": "repnum2016"
+	}, {
+		"username": "Thomas Menant",
+		"email": "Thomas.MENANT@modernisation.gouv.fr",
+		"password": "repnum2016"
+	}, {
+		"username": "Henri Verdier",
+		"email": "Henri.VERDIER@modernisation.gouv.fr",
+		"password": "repnum2016"
+	}, {
+		"username": "Florent De Bodman",
+		"email": "florent.de-bodman@cabinets.finances.gouv.fr",
+		"password": "repnum2016"
+	},
+
+	{
+		"username": "Matti Schneider",
+		"email": "matti.schneider@data.gouv.fr",
+		"password": "repnum2016"
+	}
+]);
