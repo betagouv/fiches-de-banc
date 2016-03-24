8619293eccadb53e044442ce4f1e14c6492d2557
Matti Schneider <matti.schneider@data.gouv.fr>
2016-03-24 12:04:08 +0100

    Improve discoverability of Users setup
    Add user fixtures

--- server/fixtures/Users.js
+++ server/fixtures/Users.js
@@ -1,4 +1,27 @@
 USERS = typeof USERS !== 'undefined' ? USERS : [];
 
-if (Meteor.settings.public.demo)
-	USERS = USERS.concat(Meteor.settings.public.demo);
+if (Meteor.settings.public.demo) {
+	USERS = USERS.concat(Meteor.settings.public.demo,
+		{
+			"username": "Marine Desbord",
+			"email": "marine.desbord@fake.gouv.fr",
+			"password": "kljndxfihà_-sdfskjdfngl"
+		}, {
+			"username": "Thomas Joulier",
+			"email": "thomas.joulier@fake.gouv.fr",
+			"password": "dkvbjhxdkjbhnsglnsrlkjbxcvb234"
+		}, {
+			"username": "Fazim Wallid",
+			"email": "fazim.wallid@fake.gouv.fr",
+			"password": "dlkbhjvdkjl23jpEKJZ03940"
+		}, {
+			"username": "Kinta Moulié",
+			"email": "kinta.moulie@fake.gouv.fr",
+			"password": "lmksjvlmkjmklj213_°PKRP34I"
+		}, {
+			"username": "Carine Ponti",
+			"email": "carine.ponti@fake.gouv.fr",
+			"password": "fjklvhLMKJ2A309123U4JKNR"
+		}
+	);
+}
---
+++ server/private/Users.example.js
@@ -0,0 +1,9 @@
+USERS = typeof USERS !== 'undefined' ? USERS : [];
+
+USERS = USERS.concat([
+	// {
+	// 	"username": "Marine Desbord",
+	// 	"email": "marine.desbord@fake.gouv.fr",
+	// 	"password": "kljndxfihà_-sdfskjdfngl"
+	// }
+]);
